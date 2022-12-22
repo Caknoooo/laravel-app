0ba3c527357b160b3a57223a3c384d109415520d
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-22 14:12:52 +0700

    refactor: modified some files and testing without blade post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cakno Blog | Home</title>
-</head>
-<body>
-    <h1>Home</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+    <h1>Hallo</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Caknoo",
         "email" => "caknocomel@gmail.com",
         "jurusan" => "Teknik Informatika",
@@ -27,7 +30,55 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_post = [
+        [
+            "title" => "Judul 1",
+            "slug" => "judul-1",
+            "author" => "Caknoo",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa magnam, omnis, explicabo similique velit officiis in autem quam, corporis ipsa ab dolorum recusandae repudiandae accusantium nihil aliquid voluptatum ut. Inventore recusandae vel illo beatae quas natus dignissimos quod quisquam, eligendi vitae cumque id est eos perspiciatis fugit quaerat perferendis officia? Unde iusto ad numquam architecto molestiae doloribus. Repellendus esse illo sint pariatur hic eum distinctio explicabo quis id aspernatur est a, possimus consequuntur, inventore iste maxime. Aut, necessitatibus ut?"
+        ],
+        [
+            "title" => "Judul 2",
+            "slug" => "judul-2",
+            "author" => "Caknii",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa magnam, omnis, explicabo similique velit officiis in autem quam, corporis ipsa ab dolorum recusandae repudiandae accusantium nihil aliquid voluptatum ut. Inventore recusandae vel illo beatae quas natus dignissimos quod quisquam, eligendi vitae cumque id est eos perspiciatis fugit quaerat perferendis officia? Unde iusto ad numquam architecto molestiae doloribus. Repellendus esse illo sint pariatur hic eum distinctio explicabo quis id aspernatur est a, possimus consequuntur, inventore iste maxime. Aut, necessitatibus ut?"
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_post
+    ]);
+});
+
+// Halaman single post
+Route::get('/posts/{slug}', function($slug){
+    $blog_post = [
+        [
+            "title" => "Judul 1",
+            "slug" => "judul-1",
+            "author" => "Caknoo",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa magnam, omnis, explicabo similique velit officiis in autem quam, corporis ipsa ab dolorum recusandae repudiandae accusantium nihil aliquid voluptatum ut. Inventore recusandae vel illo beatae quas natus dignissimos quod quisquam, eligendi vitae cumque id est eos perspiciatis fugit quaerat perferendis officia? Unde iusto ad numquam architecto molestiae doloribus. Repellendus esse illo sint pariatur hic eum distinctio explicabo quis id aspernatur est a, possimus consequuntur, inventore iste maxime. Aut, necessitatibus ut?"
+        ],
+        [
+            "title" => "Judul 2",
+            "slug" => "judul-2",
+            "author" => "Caknii",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa magnam, omnis, explicabo similique velit officiis in autem quam, corporis ipsa ab dolorum recusandae repudiandae accusantium nihil aliquid voluptatum ut. Inventore recusandae vel illo beatae quas natus dignissimos quod quisquam, eligendi vitae cumque id est eos perspiciatis fugit quaerat perferendis officia? Unde iusto ad numquam architecto molestiae doloribus. Repellendus esse illo sint pariatur hic eum distinctio explicabo quis id aspernatur est a, possimus consequuntur, inventore iste maxime. Aut, necessitatibus ut?"
+        ]
+    ];
+    
+    $new_post = [];
+    foreach($blog_post as $post){
+        if($post['slug'] === $slug){
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single post",
+        "post" => $new_post
+    ]);
 });
 
 
