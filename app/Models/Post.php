41dbb3ae1673e854ffc058095c6d685fa3259b43
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-23 18:27:01 +0700

    feat: added new post_model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,30 +2,17 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul 1",
-            "slug" => "judul-1",
-            "author" => "Caknoo",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa magnam, omnis, explicabo similique velit officiis in autem quam, corporis ipsa ab dolorum recusandae repudiandae accusantium nihil aliquid voluptatum ut. Inventore recusandae vel illo beatae quas natus dignissimos quod quisquam, eligendi vitae cumque id est eos perspiciatis fugit quaerat perferendis officia? Unde iusto ad numquam architecto molestiae doloribus. Repellendus esse illo sint pariatur hic eum distinctio explicabo quis id aspernatur est a, possimus consequuntur, inventore iste maxime. Aut, necessitatibus ut?"
-        ],
-        [
-            "title" => "Judul 2",
-            "slug" => "judul-2",
-            "author" => "Caknii",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae culpa magnam, omnis, explicabo similique velit officiis in autem quam, corporis ipsa ab dolorum recusandae repudiandae accusantium nihil aliquid voluptatum ut. Inventore recusandae vel illo beatae quas natus dignissimos quod quisquam, eligendi vitae cumque id est eos perspiciatis fugit quaerat perferendis officia? Unde iusto ad numquam architecto molestiae doloribus. Repellendus esse illo sint pariatur hic eum distinctio explicabo quis id aspernatur est a, possimus consequuntur, inventore iste maxime. Aut, necessitatibus ut?"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all(){
-        return collect(self::$blog_posts);
-    }
+    // Fillable bertujuan untuk memasukkan data sesuai dengan nama kolomnya
+    protected $fillable = ['title', 'excerpt', 'body'];
 
-    public static function find($slug){
-        $posts = static::all();
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    // Guarded bertujuan untuk memasukkan data selain dengan nama yang dimasukkan
+    // ex : masukkan smua data ke kolom yang ada kecuali ID
+    protected $guarded = ['id'];
 }
