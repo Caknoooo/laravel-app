3378f5c7cb5c13734cff5946d082e45ea00727fb
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-24 07:26:39 +0700

    refactor: change the code and synchronize it

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PDO;
 
 class Post extends Model
 {
     use HasFactory;
 
     // Fillable bertujuan untuk memasukkan data sesuai dengan nama kolomnya
-    protected $fillable = ['title', 'excerpt', 'body'];
+    //protected $fillable = ['title', 'excerpt', 'body'];
 
     // Guarded bertujuan untuk memasukkan data selain dengan nama yang dimasukkan
     // ex : masukkan smua data ke kolom yang ada kecuali ID
     protected $guarded = ['id'];
+
+    // Relasi one to one terhadap class category
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
@@ -25,4 +26,18 @@ Route::get('/posts', [PostController::class, 'index']);
 // Halaman single post
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
+// Halaman category relationship
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
 
+Route::get('/categories', function(){
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
