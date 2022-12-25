df0580093fff69122a247d236d1071d74682c188
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-25 16:43:59 +0700

    refactor: modify some files to redesign_ui to make it look more attractive

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
-    <section>
-        <h2>{{ $post->title }}</h2>
-        <h5>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
-        {!! $post->body !!}
-    </section>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h1 class="mb-3">{{ $post->title }}</h1>
+                <h5>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
 
-    <a class="btn btn-primary mt-3 " href="/posts" role="button">Back</a>
+                <img src="https://source.unsplash.com/1200x500?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+                <article class="my-2 fs-6">
+                    {!! $post->body !!}
+                </article>
+                
+                <a class="btn btn-primary mt-3 " href="/posts" role="button">Back</a>
+            </div>
+        </div>
+    </div>
 @endsection
