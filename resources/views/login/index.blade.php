b13e91d2ad36f5f32be0e8eae346542d3235946a
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-12-28 09:09:54 +0700

    refactor: modify views to synchronize with controllers and routes

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,34 +2,48 @@
 
 @section('container')
 
-@if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show " role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
+        @if(session()->has('loginError'))
+            <div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+                {{ session('loginError') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
     </div>
-@endif
+</div>
 
 <div class="login-body">
     <div class="box">
         <div class="form">
-            <h2>
-                Sign in
-            </h2>
-            <div class="inputBox">
-                <input type="text" required="required">
-                    <span>Username</span>
-                    <i></i>
-            </div>
-            <div class="inputBox">
-                <input type="password" required="required">
-                    <span>Password</span>
-                    <i></i>
-            </div>
-            <div class="links">
-                <a href="#">Forgot Password</a>
-                <a href="/register">Register</a>
-            </div>
-            <input type="submit" value="Login">
+            <form action="/login" method="post">
+                @csrf
+                <h2>
+                    Sign in
+                </h2>
+                <div class="inputBox">
+                    <input type="email" name="email" id="email" autofocus required="required" class="@error ('email') is-invalid @enderror" value="{{ old('email') }}">
+                        <span>Email</span>
+                        <i></i>
+                </div>
+                <div class="inputBox">
+                    <input type="password" name="password" id="password" required="required">
+                        <span>Password</span>
+                        <i></i>
+                </div>
+                <div class="links">
+                    <a href="#">Forgot Password</a>
+                    <a href="/register">Register</a>
+                </div>
+                <input type="submit" value="Login">
+            </form>
         </div>
     </div>
 </div>
