eef7d8c72a7fcea3f5aefd5a91c2ea650169e0cf
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-02 08:48:33 +0700

    refactor: updated posts and blog UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,15 @@
         <div class="row justify-content-center mb-5">
             <div class="col-md-8">
                 <h1 class="mb-3">{{ $post->title }}</h1>
-                <h5>By. <a href="/posts{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
+                <h5>By. <a href="/posts {{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }} </a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> </h5>
 
-                <img src="https://source.unsplash.com/1200x500?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
-
+                @if($post->image)
+                    <div  style="max-height: 350px; overflow: hidden;">
+                        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid">
+                    </div>
+                @else 
+                    <img src="https://source.unsplash.com/1200x500?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+                @endif
                 <article class="my-2 fs-6">
                     {!! $post->body !!}
                 </article>
